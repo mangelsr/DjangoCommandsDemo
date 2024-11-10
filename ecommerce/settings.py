8af5a2aaabf8f18481ea9db1c0b5d381842fe3cb
Miguel Sanchez <mangelsr25@gmail.com>
2024-11-10 14:40:35 -0500

    [UPD] Settings file to include added modules apps

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.products",
+    "apps.customers",
+    "apps.orders",
+    "apps.inventories",
 ]
 
 MIDDLEWARE = [
