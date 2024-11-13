358dbc7d41b0be0da6339b7ae3fb5e4856f48695
Miguel Sanchez <miguel.sanchez@devsu.com>
2024-11-13 18:08:41 -0500

    [FIX] Model registration on admin site

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import Order, OrderItem
 
-admin.register(Order)
-admin.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- apps/products/admin.py
+++ apps/products/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.register(Category)
-admin.register(Product)
+admin.site.register(Category)
+admin.site.register(Product)
